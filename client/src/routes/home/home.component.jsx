70c8655ec36707419084bf6b922778bca8912537
salvadorf8 <aztecsalvador@gmail.com>
2023-01-01 00:26:56 -0600

    socket.io library - inital code to get it working

--- client/src/routes/home/home.component.jsx
+++ client/src/routes/home/home.component.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { io } from 'socket.io-client';
 
 import UserSearch from './components/users-search.component';
 import ChatArea from './components/chat-area.component';
 import UsersList from './components/users-list.component';
 
 const Home = () => {
+    const socket = io('http://localhost:5000');
     const [searchKey, setSearchKey] = useState('');
     const { selectedChat } = useSelector((state) => state.userReducer);
 
--- server/index.js
+++ server/index.js
@@ -8,12 +8,28 @@ const messagesRoute = require('./routes/messages.route');
 
 const app = express();
 app.use(express.json());
+
+// socket.io initialization
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+});
+
+// check the connection of socket from client
+io.on('connection', (socket) => {
+    // socket events will be here
+    console.log('connected with socketid', socket.id);
+});
+
 app.use('/api/users', usersRoute);
 app.use('/api/chats', chatsRoute);
 app.use('/api/messages', messagesRoute);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('listening on 5000');
 });
